42bd927acce5f5ac1fa6d53b21a5fbe5a711ed57
Maurice Mangala <ymangala39@gmail.com>
2021-02-05 11:58:36 +0100

    Updates

---
+++ precache-manifest.42ccde4818c2a4a358120a831368a8f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ad9ac0322beb04e818b5c727717c7a",
+    "url": "/blog-react/index.html"
+  },
+  {
+    "revision": "ac4ea9e139bdccbe454e",
+    "url": "/blog-react/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "4c58715bf5e16471eb34",
+    "url": "/blog-react/static/css/main.f8c63522.chunk.css"
+  },
+  {
+    "revision": "ac4ea9e139bdccbe454e",
+    "url": "/blog-react/static/js/2.1c55b5ad.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/blog-react/static/js/2.1c55b5ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c58715bf5e16471eb34",
+    "url": "/blog-react/static/js/main.39ca4a1a.chunk.js"
+  },
+  {
+    "revision": "ffd7f40d994a79225008",
+    "url": "/blog-react/static/js/runtime-main.144e60cd.js"
+  }
+]);
